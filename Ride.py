11d0fad40073c0c37124fb760b202c553101de27
Maison <lucas.maison@telecom-parsitech.fr>
2018-03-01 19:52:14 +0100

    corrections

--- Ride.py
+++ Ride.py
@@ -18,7 +18,7 @@ class Ride():
         self.yStart = L[1]
         self.xEnd = L[2]
         self.yEnd = L[3]
-        self.earlyStart L[4]
+        self.earlyStart = L[4]
         self.latestFinish = L[5]
         self.time = abs(xStart - xEnd) + abs(yStart - yEnd)
         self.latestStart = latestFinish - time
