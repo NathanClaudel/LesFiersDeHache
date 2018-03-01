1ab872829a0c1b5e6cb29b48e839e845d565f8fc
Louison Braun <louison.braun@hotmail.fr>
2018-03-01 20:38:11 +0100

    changement map et imports

--- Ride.py
+++ Ride.py
@@ -17,8 +17,8 @@ class Ride():
     def __init__(self, L, id_number):
         self.posStart = (L[0], L[1])
         self.posEnd = (L[2], L[3])
-        self.earlyStart L[4]
+        self.earlyStart = L[4]
         self.latestFinish = L[5]
-        self.time = abs(xStart - xEnd) + abs(yStart - yEnd)
-        self.latestStart = latestFinish - time
+        self.time = abs(self.posStart[0] - self.posEnd[0]) + abs(self.posStart[1] - self.posEnd[1])
+        self.latestStart = self.latestFinish - self.time
         self.id = id_number
