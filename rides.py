0f199efcc8e501a63ed46a335573d4b84fb1dcac
Louison Braun <louison.braun@hotmail.fr>
2018-03-01 19:17:47 +0100

    creation code

---
+++ rides.py
@@ -0,0 +1,8 @@
+fichier = open("a_example.in", "r")
+constants = fichier.readline().rstrip().split(' ')
+rows = constants[0]
+columns = constants[1]
+nbVehic = constants[2]
+nbRides = constants[3]
+bonus = constants[4]
+timeMax = constants[5]
